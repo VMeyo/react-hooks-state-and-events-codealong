4a43211fe7994f1b814f3f198c018e2dcdf81517
Victor <victor.meyo@student.moringaschool.com>
2025-03-06 18:01:18 +0300

    All tests pass

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,26 @@
-import React from "react";
+/*import React from "react";
 
 function Toggle() {
   return <button>OFF</button>;
+}
+
+export default Toggle;*/
+
+
+
+import React , { useState } from 'react';
+
+function Toggle(){
+  const [isOn, setIsOn] = useState(false);
+
+  function handleClick(){
+    setIsOn((isOn) => !isOn);
+  }
+
+  const color = isOn ? "red" : "white";
+
+  <button style={{ background: color }} onClick={handleClick}>{isOn ? 'ON' : 'OFF'}</button>
+
 }
 
 export default Toggle;
